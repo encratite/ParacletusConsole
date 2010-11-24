acacaab5ac5b2d64daa892f3bc92418f84dd2c15
void <git.email.address@googlemail.com>
2010-11-24 23:38:58 +0100

    Changed the looks a bit to make it feel more like a regular terminal

--- ParacletusConsole/ParacletusForm.Designer.cs
+++ ParacletusConsole/ParacletusForm.Designer.cs
@@ -34,18 +34,25 @@
 			// 
 			// consoleBox
 			// 
+			this.consoleBox.BackColor = System.Drawing.Color.Black;
+			this.consoleBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.consoleBox.ForeColor = System.Drawing.Color.White;
 			this.consoleBox.Location = new System.Drawing.Point(0, 1);
 			this.consoleBox.Name = "consoleBox";
 			this.consoleBox.ReadOnly = true;
+			this.consoleBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
 			this.consoleBox.Size = new System.Drawing.Size(630, 426);
 			this.consoleBox.TabIndex = 1;
 			this.consoleBox.Text = "";
 			// 
 			// inputBox
 			// 
-			this.inputBox.Location = new System.Drawing.Point(0, 433);
+			this.inputBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.inputBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.inputBox.ForeColor = System.Drawing.Color.White;
+			this.inputBox.Location = new System.Drawing.Point(10, 433);
 			this.inputBox.Name = "inputBox";
-			this.inputBox.Size = new System.Drawing.Size(633, 18);
+			this.inputBox.Size = new System.Drawing.Size(610, 11);
 			this.inputBox.TabIndex = 0;
 			this.inputBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.inputBox_KeyPress);
 			// 
@@ -53,6 +60,7 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 11F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Black;
 			this.ClientSize = new System.Drawing.Size(632, 453);
 			this.Controls.Add(this.inputBox);
 			this.Controls.Add(this.consoleBox);
