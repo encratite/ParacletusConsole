0ad1fb4eee89901d45cc0dbcae750f411437ff0c
void <git.email.address@googlemail.com>
2010-12-03 16:41:34 +0100

    Working on the scriptable interface

---
+++ ParacletusConsole/EmbeddedScripting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.CodeDom.Compiler;
+
+namespace ParacletusConsole
+{
+	class EmbeddedScripting
+	{
+		Microsoft.CSharp.CSharpCodeProvider CodeProvider;
+		CompilerParameters Parameters;
+
+		public EmbeddedScripting()
+		{
+			CodeProvider = new Microsoft.CSharp.CSharpCodeProvider();
+			Parameters = new CompilerParameters();
+			Parameters.GenerateExecutable = false;
+			Parameters.GenerateInMemory = true;
+			Parameters.ReferencedAssemblies.Add(Assembly.GetExecutingAssembly().Location);
+		}
+
+		CompilerResults Compile(string code)
+		{
+			return CodeProvider.CompileAssemblyFromSource(Parameters, code);
+		}
+	}
+}
---
+++ ParacletusConsole/EmbeddedScriptingException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.CodeDom.Compiler;
+
+namespace ParacletusConsole
+{
+	class EmbeddedScriptingException: Exception
+	{
+		public CompilerErrorCollection Errors;
+
+		public EmbeddedScriptingException(CompilerErrorCollection errors)
+		{
+			Errors = errors;
+		}
+	}
+}
